a411d0355903e9c1eed9a60ca885b2320905b316
LeeWhite187 <LeeWhite187@gmail.com>
2023-03-13 04:54:38 -0400

    Added simple method in dev project, to generate a keystore.

--- OGA.KeyMgmt/OGA.KeyMgmt_Dev/Program.cs
+++ OGA.KeyMgmt/OGA.KeyMgmt_Dev/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using OGA.KeyMgmt.Model;
+using OGA.KeyMgmt.Store;
+using System;
+using System.Text;
 
 namespace OGA.KeyMgmt.Dev
 {
@@ -7,6 +10,43 @@ namespace OGA.KeyMgmt.Dev
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Generate_Keystore();
+        }
+
+        static public void Generate_Keystore()
+        {
+            // Accept the current encryption key...
+            string rtkey = "enter_key_here";
+
+
+            string storagepassword = rtkey;
+
+            // Create a test key...
+            var k1 = new KeyObject_v2();
+            k1.KeyName = Guid.NewGuid().ToString();
+            k1.KeyType = eKeyType.AES.ToString();
+            k1.CreationUTC = DateTime.UtcNow;
+            k1.LastUpdateUTC = k1.CreationUTC;
+            k1.Status = eKeyStatus.Enabled;
+            k1.KeyLength = 256;
+            k1.HasPrivateKey = true;
+            k1.PrivateKey = Nanoid.Nanoid.Generate( size:32);
+            k1.PrivEncrypted = false;
+            k1.PublicKey = "";
+            k1.HasPublicKey = false;
+            k1.IsBase64Encoded = false;
+            k1.Uses = Guid.NewGuid().ToString();
+
+            // Add the key to the store...
+            var ks = new KeyStore_v2_JsonConfig(storagepassword);
+            var res = ks.AddKey_toStore(k1);
+
+            var res2 = ks.Save(out var config);
+
+            string jsonconfig = Newtonsoft.Json.JsonConvert.SerializeObject(config, Newtonsoft.Json.Formatting.Indented);
+
+            int x = 0;
         }
     }
 }
