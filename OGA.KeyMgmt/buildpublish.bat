68d1e84ce61d1307446a8949c3dbd0cdea5ad361
LeeWhite187 <LeeWhite187@gmail.com>
2024-03-13 07:26:50 -0400

    Updated library references. Added NET7 output target. Added warning suppression for NET5. +semver:minor

--- OGA.KeyMgmt/buildpublish.bat
+++ OGA.KeyMgmt/buildpublish.bat
@@ -4,14 +4,20 @@ REM Build the library...
 dotnet restore "./OGA.KeyMgmt_NET5/OGA.KeyMgmt_NET5.csproj"
 dotnet build "./OGA.KeyMgmt_NET5/OGA.KeyMgmt_NET5.csproj" -c DebugLinux --runtime linux-x64 --no-self-contained
 
-dotnet restore "./OGA.KeyMgmt_NET6/OGA.KeyMgmt_NET6.csproj"
-dotnet build "./OGA.KeyMgmt_NET6/OGA.KeyMgmt_NET6.csproj" -c DebugLinux --runtime linux-x64 --no-self-contained
-
 dotnet restore "./OGA.KeyMgmt_NET5/OGA.KeyMgmt_NET5.csproj"
 dotnet build "./OGA.KeyMgmt_NET5/OGA.KeyMgmt_NET5.csproj" -c DebugWin --runtime win-x64 --no-self-contained
 
 dotnet restore "./OGA.KeyMgmt_NET6/OGA.KeyMgmt_NET6.csproj"
+dotnet build "./OGA.KeyMgmt_NET6/OGA.KeyMgmt_NET6.csproj" -c DebugLinux --runtime linux-x64 --no-self-contained
+
+dotnet restore "./OGA.KeyMgmt_NET6/OGA.KeyMgmt_NET6.csproj"
 dotnet build "./OGA.KeyMgmt_NET6/OGA.KeyMgmt_NET6.csproj" -c DebugWin --runtime win-x64 --no-self-contained
+
+dotnet restore "./OGA.KeyMgmt_NET7/OGA.KeyMgmt_NET7.csproj"
+dotnet build "./OGA.KeyMgmt_NET7/OGA.KeyMgmt_NET7.csproj" -c DebugLinux --runtime linux-x64 --no-self-contained
+
+dotnet restore "./OGA.KeyMgmt_NET7/OGA.KeyMgmt_NET7.csproj"
+dotnet build "./OGA.KeyMgmt_NET7/OGA.KeyMgmt_NET7.csproj" -c DebugWin --runtime win-x64 --no-self-contained
 
 REM Create the composite nuget package file from built libraries...
 C:\Programs\nuget\nuget.exe pack ./OGA.KeyMgmt.nuspec -IncludeReferencedProjects -symbols -SymbolPackageFormat snupkg -OutputDirectory ./Publish -Verbosity detailed
