1ff9f8c425d57bd00eaf93e4a82ba537a0b81b49
LeeWhite187 <LeeWhite187@gmail.com>
2024-03-27 21:56:32 -0400

    Updated library references. Expanded runtime identifier to apply to win-all and linux-all. +semver:minor

--- OGA.KeyMgmt/buildpublish.bat
+++ OGA.KeyMgmt/buildpublish.bat
@@ -2,22 +2,22 @@ REM OGA Key Management Library
 
 REM Build the library...
 dotnet restore "./OGA.KeyMgmt_NET5/OGA.KeyMgmt_NET5.csproj"
-dotnet build "./OGA.KeyMgmt_NET5/OGA.KeyMgmt_NET5.csproj" -c DebugLinux --runtime linux-x64 --no-self-contained
+dotnet build "./OGA.KeyMgmt_NET5/OGA.KeyMgmt_NET5.csproj" -c DebugLinux --runtime linux --no-self-contained
 
 dotnet restore "./OGA.KeyMgmt_NET5/OGA.KeyMgmt_NET5.csproj"
-dotnet build "./OGA.KeyMgmt_NET5/OGA.KeyMgmt_NET5.csproj" -c DebugWin --runtime win-x64 --no-self-contained
+dotnet build "./OGA.KeyMgmt_NET5/OGA.KeyMgmt_NET5.csproj" -c DebugWin --runtime win --no-self-contained
 
 dotnet restore "./OGA.KeyMgmt_NET6/OGA.KeyMgmt_NET6.csproj"
-dotnet build "./OGA.KeyMgmt_NET6/OGA.KeyMgmt_NET6.csproj" -c DebugLinux --runtime linux-x64 --no-self-contained
+dotnet build "./OGA.KeyMgmt_NET6/OGA.KeyMgmt_NET6.csproj" -c DebugLinux --runtime linux --no-self-contained
 
 dotnet restore "./OGA.KeyMgmt_NET6/OGA.KeyMgmt_NET6.csproj"
-dotnet build "./OGA.KeyMgmt_NET6/OGA.KeyMgmt_NET6.csproj" -c DebugWin --runtime win-x64 --no-self-contained
+dotnet build "./OGA.KeyMgmt_NET6/OGA.KeyMgmt_NET6.csproj" -c DebugWin --runtime win --no-self-contained
 
 dotnet restore "./OGA.KeyMgmt_NET7/OGA.KeyMgmt_NET7.csproj"
-dotnet build "./OGA.KeyMgmt_NET7/OGA.KeyMgmt_NET7.csproj" -c DebugLinux --runtime linux-x64 --no-self-contained
+dotnet build "./OGA.KeyMgmt_NET7/OGA.KeyMgmt_NET7.csproj" -c DebugLinux --runtime linux --no-self-contained
 
 dotnet restore "./OGA.KeyMgmt_NET7/OGA.KeyMgmt_NET7.csproj"
-dotnet build "./OGA.KeyMgmt_NET7/OGA.KeyMgmt_NET7.csproj" -c DebugWin --runtime win-x64 --no-self-contained
+dotnet build "./OGA.KeyMgmt_NET7/OGA.KeyMgmt_NET7.csproj" -c DebugWin --runtime win --no-self-contained
 
 REM Create the composite nuget package file from built libraries...
 C:\Programs\nuget\nuget.exe pack ./OGA.KeyMgmt.nuspec -IncludeReferencedProjects -symbols -SymbolPackageFormat snupkg -OutputDirectory ./Publish -Verbosity detailed
